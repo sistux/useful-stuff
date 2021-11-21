32076905085ba32b4102e0ba0920d1e9d036ca3d
Sergej Stepanov <Sergej.Stepanov@bruker.com>
2021-11-21 14:31:52 +0100

    - updated script to run it as a service - systemd-file - readme file

--- rpi3/fanctrl.sh
+++ rpi3/fanctrl.sh
@@ -6,50 +6,93 @@
 # if it's higher than threshold it starts the fan
 #
 
-TEMPTHRESHOLDVAL=38 # temperature
+TEMPTHRESHOLDVAL=40 # temperature
+TEMPHISTER=3 # histeresis
 FANGPIO=4
 CHECKDELAY=5
-
-# prepare the gpio
+FANGPIOPIDFILE=/var/run/fanctrl.pid
 GPIODIR="/sys/class/gpio"
 FANGPIOPATH=$GPIODIR"/gpio"$FANGPIO
 
-echo $GPIODIR
-echo $FANGPIOPATH
-
-if [ ! -e "$FANGPIOPATH/value" ];then
-    echo $FANGPIO > $GPIODIR/export
-    sleep 1
-    if [ ! -e "$FANGPIOPATH/direction" ];then
-	echo -e "[ERROR] GPIO '$FANGPIO' cann't be exported"
-	exit 1
-    else
-	ls -l $GPIODIR
-
-	echo
-	ls -l $FANGPIOPATH
-	echo "testt echo out > $FANGPIOPATH"
-	#ls -l $FANGPIOPATH/direction
-	#ls -l $FANGPIOPATH/value
-	echo out>$FANGPIOPATH/direction
-	echo 0>$FANGPIOPATH/value
+prepare_gpio()
+{
+    if [ ! -e "$FANGPIOPATH/value" ];then
+	echo $FANGPIO > $GPIODIR/export
+	sleep 1 # the system needs some time
+	if [ ! -e "$FANGPIOPATH/direction" ];then
+	    echo -e "[ERROR] GPIO '$FANGPIO' cann't be exported"
+	    exit 1
+	else
+	    echo out>$FANGPIOPATH/direction
+	    echo 0>$FANGPIOPATH/value
+	fi
     fi
-fi
+}
 
-while true
-do
-    GPIOSTATE=`cat $FANGPIOPATH/value`
-    CPUTEMP=`vcgencmd measure_temp | cut -c6,7`
-    echo T:$CPUTEMP GPIO:$GPIOSTATE
-    if [ $CPUTEMP -ge $TEMPTHRESHOLDVAL ] && [ $GPIOSTATE -eq 0 ]
-    then
-	echo "1" > $FANGPIOPATH/value
-    elif [ $CPUTEMP -le $TEMPTHRESHOLDVAL ] && [ $GPIOSTATE -eq 1 ]
-    then
-	echo "0" > $FANGPIOPATH/value
+cleanup_gpio()
+{
+    if [ -e "$FANGPIOPATH/value" ];then
+	echo $FANGPIO > $GPIODIR/unexport
     fi
+}
 
-    sleep $CHECKDELAY
-done
-echo "Exit. Normally it should not happens"
-exit 2
+runasdaemon()
+{
+    # echo "runasdaemon"
+    while true
+    do
+	GPIOSTATE=`cat $FANGPIOPATH/value`
+	CPUTEMP=`vcgencmd measure_temp | cut -c6,7`
+	if [ $CPUTEMP -ge $TEMPTHRESHOLDVAL ] && [ $GPIOSTATE -eq 0 ]
+	then
+	    # echo Enable: T:$CPUTEMP GPIO:$GPIOSTATE
+	    echo "1" > $FANGPIOPATH/value
+	elif [ $CPUTEMP -le `expr $TEMPTHRESHOLDVAL - $TEMPHISTER` ] && [ $GPIOSTATE -eq 1 ]
+	then
+	    # echo Disable: T:$CPUTEMP GPIO:$GPIOSTATE
+	    echo "0" > $FANGPIOPATH/value
+	fi
+
+	sleep $CHECKDELAY
+	# echo "runasdaemon loop"
+    done
+    echo "Exit. Normally it should not happens"
+    exit 2
+}
+
+case $1 in
+    start )
+	prepare_gpio
+	$0 runit &
+	echo $! > $FANGPIOPIDFILE
+	;;
+    stop )
+	if [ -e $FANGPIOPIDFILE ];then
+	    kill `cat $FANGPIOPIDFILE`
+	    rm $FANGPIOPIDFILE
+	fi
+	cleanup_gpio
+	;;
+    restart )
+	cleanup_gpio
+	;;
+    status)
+	# code to check status of app comes here
+	# example: status program_name
+	if [ -e $FANGPIOPIDFILE ];then
+	    echo "PID FanCtrl Process:`cat $FANGPIOPIDFILE`"
+	else
+	    echo "No PidFile \"$FANGPIOPIDFILE\" was found!"
+	fi
+	GPIOVALUE=`if [ -e $FANGPIOPATH/value ];then cat $FANGPIOPATH/value;else echo "nogpio";fi`
+	CPUTEMP=`vcgencmd measure_temp`
+	echo "Info:cpu-temp:$CPUTEMP,gpio:$GPIOVALUE"
+	;;
+    runit )
+	runasdaemon
+	;;
+    *)
+	echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0
